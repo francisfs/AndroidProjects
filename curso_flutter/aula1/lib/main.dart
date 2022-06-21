f4cf65a54ae31ef354ff49c50adbf82d72535f57
francisfs <francis_2.5@hotmail.com>
2022-06-21 00:12:37 -0300

    4opçoes

---
+++ curso_flutter/aula1/lib/main.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'app_widget.dart';
+
+void main() {
+  runApp(AppWidget()
+  
+  );
+}
+
+
+
